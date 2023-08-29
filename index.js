0254ac1912f6ecedabf905108dd5735d2ff8ad98
alexc <alexcolwell13@gmail.com>
2023-08-29 12:30:06 -0500

    set up basic layout for inquirer to ask question, and answer to be generated in a  readme

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
+const fs = require("fs")
+const inquirer = require("inquirer")
+const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is a title of you project?"
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+        err ? console.log(err) : console.log("README.md generated successfully!")
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        writeToFile("README.md", generateMarkdown(data))
+    })
+}
 
 // Function call to initialize app
-init();
+init()
