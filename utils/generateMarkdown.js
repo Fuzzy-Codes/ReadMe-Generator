fbb01c1fd4344eea098a9637b579d5481ab5f429
alexc <alexcolwell13@gmail.com>
2023-08-29 12:52:11 -0500

    set up the rest of render license functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,39 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(data) {
+  if(data.license === "None") {
+    return ""
+  } else if (data.license === "MIT") {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  } else if (data.license === "Apache") {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  } else if (data.license === "GNU GPLv2") {
+    return `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`
+  } else if (data.license === "Boost") {
+    return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(data) {
+  if(data.license === "None") {
+    return ""
+  } else {
+    return `* [License](#license)`
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(data) {
+  if(data.license === "None") {
+    return ""
+  } else {
+    return `## License
+${renderLicenseBadge(data)}`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -21,7 +46,7 @@ ${data.description}
 * [Installation](#installation)
 * [Usage](#usage)
 * [Credits] (#credits)
-* [License](#license)
+${renderLicenseLink(data)}
 
 ## Installation
 
@@ -29,9 +54,7 @@ ${data.description}
 
 ## Credits
 
-## License
-
-
+${renderLicenseSection(data)}
 `;
 }
 
